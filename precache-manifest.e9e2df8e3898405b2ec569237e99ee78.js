9bd539f360660efa2c00c795e9ee694a7279e9e3
ufoo68 <mitarou.yuutarou@gmail.com>
2020-04-18 14:02:49 +0900

    Updates

---
+++ precache-manifest.e9e2df8e3898405b2ec569237e99ee78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f2e85663d47d8266f215159268fe7f",
+    "url": "/play-alexa/index.html"
+  },
+  {
+    "revision": "77aed2c5180c15453832",
+    "url": "/play-alexa/static/css/main.6ee340c7.chunk.css"
+  },
+  {
+    "revision": "6f777635a6b51194a878",
+    "url": "/play-alexa/static/js/2.e3112e45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/play-alexa/static/js/2.e3112e45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77aed2c5180c15453832",
+    "url": "/play-alexa/static/js/main.3479b377.chunk.js"
+  },
+  {
+    "revision": "97343227584e1e1fdb8e",
+    "url": "/play-alexa/static/js/runtime-main.0d15330e.js"
+  }
+]);
