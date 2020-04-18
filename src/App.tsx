89d25d4904348adebb730d0af026813a76119567
ufoo68 <mitarou.yuutarou@gmail.com>
2020-04-18 14:40:16 +0900

    Implemented radio taisou

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+const m4a = require('./assets/playAlexa.m4a')
+
+const audio = new Audio(m4a)
+const playAlexa = () => audio.play()
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={playAlexa} className="playButton">ラジオ体操</button>
     </div>
   );
 }
